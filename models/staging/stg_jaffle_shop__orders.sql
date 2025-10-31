6c5d07632d2a25976e2bb2dc2ccb8dd5b3de5705
Ivan Ivanov <zakz13@gmail.com>
2025-10-31 15:51:06 +0200

    Add freshness

--- models/staging/stg_jaffle_shop__orders.sql
+++ models/staging/stg_jaffle_shop__orders.sql
@@ -2,6 +2,13 @@ select
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status,
+        _etl_loaded_at
 
-    from jaffle_shop.orders
+    from {{source('jaffle_shop', 'orders')}}
+
+
+-- select
+--         *
+
+--     from {{source('jaffle_shop', 'orders')}}
